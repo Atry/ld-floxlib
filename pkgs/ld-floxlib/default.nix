5f5aa9138ab5873b73ad64729e3361225509941d
Yang, Bo <bo.yang@protonbase.io>
2024-03-27 09:54:26 +0000

    Add `defaultLibraries`

--- pkgs/ld-floxlib/default.nix
+++ pkgs/ld-floxlib/default.nix
@@ -1,14 +1,19 @@
-{ self, lib, buildEnv, stdenv }:
+{ self
+, lib
+, buildEnv
+, stdenv
+, defaultLibraries ? [
+    # List here any libraries that you would prefer to see
+    # loaded purely from Nix.
+    stdenv.cc.cc.lib	# for libstdc++.so.6
+  ]
+}:
 
 let
   pname = "ld-floxlib";
   ld_floxlib_libs = buildEnv {
     name = "${pname}-libs";
-    paths = [
-      # List here any libraries that you would prefer to see
-      # loaded purely from Nix.
-      stdenv.cc.cc.lib	# for libstdc++.so.6
-    ];
+    paths = defaultLibraries;
   };
 
 in
